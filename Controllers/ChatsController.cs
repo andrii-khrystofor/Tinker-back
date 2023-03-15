592662baae0d9b21cfd339331348b2a9456c01d5
vladyslav.burtsevych <vladm2323@gmail.com>
2023-03-16 01:38:26 +0200

    refactor: add few fields to models logic, moved models to Models/ folder

--- Controllers/ChatsController.cs
+++ Controllers/ChatsController.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Tinker_Back;
+using Tinker_Back.Models;
 
 namespace Tinker_Back.Controllers
 {
